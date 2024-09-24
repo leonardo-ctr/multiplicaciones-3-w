7225215386b264ec776f6626f4e5a2e9793563a0
pollocandente <a.23308051281211@cbtis128.edu.mx>
2024-09-23 20:29:20 -0600

    Add files via upload

---
+++ multiplicaiones.py
@@ -0,0 +1,8 @@
+print(" ")#define una linea en blanco
+print("Roman De la Cruz Leonardo Antonio, multiplicacion de numeros ")
+numero1 = int(input("Introduce el numero1: "))#solicita el primer valor
+numero2= int(input("Introduce el numero2: "))#solicita el segundo valor
+numero3 = int(input("Introduce el numero3: "))#solicita el tercer valor
+print(" ")#define una linea en blanco
+multiplicacion=numero1*numero2*numero3#la multiplicacion de los valores que se hara
+print("la multiplicacion es", multiplicacion)#print imprime el resultado de la multiplicacion3
